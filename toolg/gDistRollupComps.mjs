c5a0041379b1ad9e502d0eb3c6c6a8e20c2e9836
yuda-lyu <firsemisphere@gmail.com>
2026-08-25 16:40:12 +0800

    modify: update pks

--- toolg/gDistRollupComps.mjs
+++ toolg/gDistRollupComps.mjs
@@ -10,10 +10,7 @@ rollupFiles({
     fdSrc,
     fdTar,
     format: 'umd',
-    //nameDistType: 'kebabCase',
-    hookNameDist: () => {
-        return 'w-echarts-vue'
-    },
+    nameDistType: 'kebabCase',
     globals: {
         'vue': 'Vue',
     },
